5b6b5d170bbae71195ce103775889acdba513fbd
Dmitriy <maradonna05@yandex.ru>
2022-09-19 15:42:59 +0300

    Finished DZ_Task 06,07

---
+++ DZ/DZ_Task06/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+bool IsThreeDigitNumber(int a)
+{
+    return a / 100 == 0;
+}
+
+if(IsThreeDigitNumber(number)) Console.WriteLine($"{number} -> Третьей цифры нет!");
+else
+{
+int ThirdDigitOfNumber(int num)
+{
+    while(num / 1000 > 0)
+    {
+        num = num / 10;
+    }
+    return num % 10;
+}
+int result = ThirdDigitOfNumber(number);
+Console.WriteLine($"{number} -> {result}!");
+}
---
+++ DZ/DZ_Task07/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите порядковый номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool IsWeekend(int num)
+{
+    return num == 6 || num == 7;
+}
+if(number < 1 || number > 7) Console.WriteLine("Такого дня недели не существует!");
+else if (IsWeekend(number)) Console.Write($"{number} -> да!");
+else Console.Write($"{number} -> нет!");
+if (IsWeekend(number)) Console.WriteLine("Выходной!");
+
