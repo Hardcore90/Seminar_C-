8d63f2e4c2e94c4377a8ec55796558a587c58665
Dmitriy <maradonna05@yandex.ru>
2022-09-22 17:18:42 +0300

    Fix DZ_Task08

--- DZ/DZ_Task08/Program.cs
+++ DZ/DZ_Task08/Program.cs
@@ -27,5 +27,5 @@ double Distance(int argX1, int argY1, int argZ1, int argX2, int argY2, int argZ2
     double res = Math.Sqrt(Math.Pow((argX2 - argX1), 2) + Math.Pow((argY2 - argY1), 2) + Math.Pow((argZ2 - argZ1), 2));
     return res;
 }
-double distance = Distance(x1, x2, y1, y2, z1, z2);
+double distance = Distance(x1, y1, z1, x2, y2, z2);
 Console.WriteLine(Math.Round(distance, 2, MidpointRounding.ToZero));
