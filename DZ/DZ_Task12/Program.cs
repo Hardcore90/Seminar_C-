024825d6a3c7068d725605517f51bbc3640d53af
Dmitriy <maradonna05@yandex.ru>
2022-09-26 13:56:54 +0300

    Finished DZ_Task12

---
+++ DZ/DZ_Task12/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int num)
+{
+    int sum = num % 10;
+    while(num / 10 > 0)
+    {
+        num = num /10;
+        sum = sum + num % 10;
+    }
+    return sum;
+}
+
+int res = SumDigit(number);
+Console.WriteLine($"{number} -> {res} !");
