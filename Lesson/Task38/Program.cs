aeedf14d1d9bd96df40c0bc206f4d2d108c5a34c
Dmitriy <maradonna05@yandex.ru>
2022-11-03 15:01:43 +0300

    Finished Task 38,39

---
+++ Lesson/Task38/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 67. Напишите программу, которая будет принимать
+// на вход число и возвращать сумму его цифр.
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int SumDigitsOfNumber(int num)
+{
+    int sum = (num % 10);
+    if(num > 0) 
+    sum = sum + SumDigitsOfNumber(num/10);
+    return sum;
+}
+int res = SumDigitsOfNumber(number);
+Console.WriteLine($"Сумма цифр числа {number} равна {res}");
---
+++ Lesson/Task39/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 69. Напишите программу, которая на вход принимает два числа
+// A и B, и возводит число A в целую степень B с помощью рекурсии.
+
+Console.WriteLine("Введите А: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int NumberAToPowOfB(int num1, int num2)
+{
+    int multiply = 1;
+    if(num2 == 0) return 1;
+    {
+        multiply = num1;
+        multiply *= NumberAToPowOfB(num1, num2-1);
+    }
+    return multiply;
+}
+int res = NumberAToPowOfB(a, b);
+Console.WriteLine($"A = {a}, B = {b} -> {res}");
