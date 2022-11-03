d020763a5b16ff72a58fb16f1b00b15df441c41d
Dmitriy <maradonna05@yandex.ru>
2022-11-03 13:33:27 +0300

    Finished Task 36,37

--- Lesson/Task36/Program.cs
+++ Lesson/Task36/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63. Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+
+Console.WriteLine("Введите N:");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if (num == 0) return;
+    else NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+Console.Write($"N = {number} -> ");
+NaturalNumbers(number);
---
+++ Lesson/Task37/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 65. Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+
+Console.WriteLine("Введите M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void NaturalNumbersFromMToN(int numM, int numN)
+{
+    if(numM < numN)
+    {
+        NaturalNumbersFromMToN(numM, numN - 1);
+        Console.Write($" {numN} ");
+    }
+    if(numM > numN)
+    {
+        Console.Write($" {numM} ");
+        NaturalNumbersFromMToN(numM - 1, numN);
+    }
+    if(numN == numM)  Console.Write($" {numM} ");
+}
+Console.Write($"M = {m}, N = {n} ->");
+NaturalNumbersFromMToN(m, n);
